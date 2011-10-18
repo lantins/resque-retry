6448a8abcc033bee31065b751d95766f4034a366
tworker <tworker@onyx.ove.com>
2011-10-18 13:22:02 -0400

    tanob - moving instance_exec into module.

--- lib/resque/plugins/retry.rb
+++ lib/resque/plugins/retry.rb
@@ -222,6 +222,17 @@ module Resque
         end
       end
 
+      def instance_exec(*args, &block)
+        mname = "__instance_exec_#{Thread.current.object_id.abs}"
+        class << self; self end.class_eval{ define_method(mname, &block) }
+        begin
+          ret = send(mname, *args)
+        ensure
+          class << self; self end.class_eval{ undef_method(mname) } rescue nil
+        end
+        ret
+      end
+
     end
   end
 end
