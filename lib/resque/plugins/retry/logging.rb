1663e72c1e18af7af11bdf10ba6ef9d1b44d54a1
Luke Antins <luke@lividpenguin.com>
2014-05-19 22:43:26 +0100

    - Add plugin "prefix" to log messages. - Log to DEBUG rather then INFO level to reduce noise during operation. - Adjust yardoc comment style to match other sections of the repo. - Remove 1 level of indentation.
    Ref: #99

--- lib/resque/plugins/retry/logging.rb
+++ lib/resque/plugins/retry/logging.rb
@@ -2,19 +2,20 @@ module Resque
   module Plugins
     module Retry
       module Logging
-        # Log messages through the Resque logger.  Generally not for application
-        # logging-just for inner-workings of Resque and plugins.
+        # Log messages through the Resque logger (DEBUG level).
+        # Generally not for application logging-just for inner-workings of
+        # Resque and plugins.
         #
-        # message:: message to log
-        # args:: args of the resque job in context
-        # exception:: the exception that might be causing a retry
+        # @param [String] message to log
+        # @param [Object] args of the resque job in context
+        # @param [Object] exception that might be causing a retry
         #
         # @api private
-        def log_message(message,args=nil,exception=nil)
-          if Resque.logger
-            exception_portion = exception.nil? ? '' : " [#{exception.class}/#{exception}]"
-            Resque.logger.info "#{args.inspect}#{exception_portion}: #{message}"
-          end
+        def log_message(message, args=nil, exception=nil)
+          return unless Resque.logger
+
+          exception_portion = exception.nil? ? '' : " [#{exception.class}/#{exception}]"
+          Resque.logger.debug "resque-retry -- #{args.inspect}#{exception_portion}: #{message}"
         end
       end
     end
