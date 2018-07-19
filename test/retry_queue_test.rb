58480071c80a665e2ac2127b2b51fa58d0cd3f37
Michael Bianco <iloveitaly@gmail.com>
2018-07-18 21:03:03 -0400

    Test for specifying retry queue dynamically

--- test/retry_queue_test.rb
+++ test/retry_queue_test.rb
@@ -13,4 +13,13 @@ class RetryQueueTest < Minitest::Test
 
     perform_next_job(@worker)
   end
+
+  def test_retry_delayed_failed_jobs_in_dynamic_queue
+    queue_name = "dynamic_queue_#{Time.now.to_i}"
+
+    Resque.enqueue(JobWithDynamicRetryQueue, queue_name)
+    Resque.expects(:enqueue_in_with_queue).with(queue_name, 1, JobWithDynamicRetryQueue, queue_name)
+
+    perform_next_job(@worker)
+  end
 end
