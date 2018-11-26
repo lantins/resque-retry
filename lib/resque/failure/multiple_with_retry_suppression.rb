ba2e496843402fb23dc6676556e2b1fd4ca874d1
Jonathan W. Zaleski <jzaleski@applause.com>
2018-11-26 08:12:26 -0500

    Monkey patch in the `requeue_queue` method onto the `Resque::Failure` backend
    This can be removed when the following PR is merged into `Resque` itself:
    
    https://github.com/resque/resque/pull/1659

--- lib/resque/failure/multiple_with_retry_suppression.rb
+++ lib/resque/failure/multiple_with_retry_suppression.rb
@@ -3,10 +3,9 @@ require 'resque/plugins/retry/logging'
 
 module Resque
   module Failure
-
     # A multiple failure backend, with retry suppression
     #
-    # For example: if you had a job that could retry 5 times, your failure 
+    # For example: if you had a job that could retry 5 times, your failure
     # backends are not notified unless the _final_ retry attempt also fails.
     #
     # Example:
@@ -34,11 +33,21 @@ module Resque
         job_being_retried = retryable && retrying?
 
         if !job_being_retried
-          log_message "#{retryable ? '' : 'non-'}retriable job is not being retried - sending failure to superclass", args_from(payload), exception
+          log_message(
+            "#{retryable ? '' : 'non-'}retryable job is not being retried - sending failure to superclass",
+            args_from(payload),
+            exception
+          )
+
           cleanup_retry_failure_log!
           super
         elsif retry_delay > 0
-          log_message "retry_delay: #{retry_delay} > 0 - saving details in Redis", args_from(payload), exception
+          log_message(
+            "retry_delay: #{retry_delay} > 0 - saving details in Redis",
+            args_from(payload),
+            exception
+          )
+
           data = {
             :failed_at => Time.now.strftime("%Y/%m/%d %H:%M:%S"),
             :payload   => payload,
@@ -48,10 +57,19 @@ module Resque
             :worker    => worker.to_s,
             :queue     => queue
           }
+          data = Resque.encode(data)
 
-          Resque.redis.setex(failure_key, 2*retry_delay, Resque.encode(data))
+          Resque.redis.setex(
+            failure_key,
+            2 * retry_delay,
+            data
+          )
         else
-          log_message "retry_delay: #{retry_delay} <= 0 - ignoring", args_from(payload), exception
+          log_message(
+            "retry_delay: #{retry_delay} <= 0 - ignoring",
+            args_from(payload),
+            exception
+          )
         end
       end
 
@@ -62,9 +80,33 @@ module Resque
         'failure-' + retry_key
       end
 
+      # Monkey-patch this in for now since it is a hard requirement for the
+      # "retry-all" functionality to work via the Web UI.
+      #
+      # This can be removed when the following PR has been merged into `Resque`
+      # itself:
+      #
+      # https://github.com/resque/resque/pull/1659
+      #
+      # @api public
+      def self.requeue_queue(queue)
+        classes.first.requeue_queue(queue)
+      end
+
       protected
 
-      # Return the class/module of the failed job.
+      def args_from(payload)
+        (payload || {})['args']
+      end
+
+      def cleanup_retry_failure_log!
+        Resque.redis.del(failure_key) if retryable?
+      end
+
+      def failure_key
+        self.class.failure_key(retry_key)
+      end
+
       def klass
         Resque::Job.new(nil, nil).constantize(payload['class'])
       end
@@ -77,10 +119,6 @@ module Resque
         klass.redis_retry_key(*payload['args'])
       end
 
-      def failure_key
-        self.class.failure_key(retry_key)
-      end
-
       def retryable?
         klass.respond_to?(:redis_retry_key)
       rescue NameError
@@ -89,14 +127,6 @@ module Resque
 
       def retrying?
         Resque.redis.exists(retry_key)
-      end
-
-      def cleanup_retry_failure_log!
-        Resque.redis.del(failure_key) if retryable?
-      end
-
-      def args_from(payload)
-        (payload || {})['args']
       end
     end
   end
